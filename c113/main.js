08a3d51ca39dc7866705ced61ac5d41dfe6eeccf
ThelegendarySpook <115424068+ThelegendarySpook@users.noreply.github.com>
2023-02-16 20:19:50 +0100

    Add files via upload

---
+++ c113/main.js
@@ -0,0 +1,19 @@
+function preload() {
+}
+
+
+
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+}
+
+
+
+
+function draw(){
+    circle(30, 30, 20);
+    ellipse(56, 46, 55, 55);
+    rect(30, 20, 55, 55);
+}
